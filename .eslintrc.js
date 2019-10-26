25c2db4c154aba828d5bb548deb58b7c76a3b440
yujinpan <1192878390@qq.com>
2019-10-26 18:22:08 +0800

    chore: 增加 @typescript-eslint 的 js 代码校验忽略项配置以及增加 eslint --fix 命令

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,21 +5,32 @@ module.exports = {
   root: true,
 
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier"  ,
-    "prettier/@typescript-eslint",
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint'
   ],
 
-  plugins: ["prettier", "@typescript-eslint"],
+  plugins: ['prettier', '@typescript-eslint'],
 
   rules: {
-    "prettier/prettier": [
-      "error",
-      { singleQuote: true, arrowParens: "always", semi: true }
+    '@typescript-eslint/no-use-before-define': ['error', {'functions': false, 'classes': false}],
+    'prettier/prettier': [
+      'error',
+      {singleQuote: true, arrowParens: 'always', semi: true}
     ],
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    eqeqeq: ["error", "always"]
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    eqeqeq: ['error', 'always']
   },
 
-  parser: "@typescript-eslint/parser"
+  parser: '@typescript-eslint/parser',
+
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off'
+      }
+    }
+  ]
 };
