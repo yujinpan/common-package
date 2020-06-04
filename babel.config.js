e45b0299cb20e9df42984f83a0ef70db05c9a235
yujinpan <1192878390@qq.com>
2020-06-04 11:06:41 +0800

    chore: add ts + babel7 plugins

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
+/**
+ * @link https://devblogs.microsoft.com/typescript/typescript-and-babel-7/
+ */
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-typescript'],
-  plugins: ['@babel/plugin-proposal-export-default-from'],
+  plugins: [
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/proposal-class-properties',
+    '@babel/proposal-object-rest-spread'
+  ],
   exclude: 'node_modules/**'
 };
