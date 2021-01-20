1274cf00f1faa814baae2c81992cf7103a3eb0c9
yujinpan <1192878390@qq.com>
2021-01-20 15:33:35 +0800

    Update rollupx.config.js

--- rollupx.config.js
+++ rollupx.config.js
@@ -4,5 +4,8 @@ module.exports = {
     ` * package-base v${require('./package.json').version}\n` +
     ` * (c) 2019-${new Date().getFullYear()} yujinpan\n` +
     ' * Released under the MIT License.\n' +
-    ' */\n'
+    ' */\n',
+  inputFiles: ['src/**/!(*.d).*(ts|js)'],
+  typesOutputDir: 'dist',
+  singleFile: false
 };
